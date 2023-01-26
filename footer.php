150a0d93208bafa284e8dbded8acb4ed2c6a43c5
saikiranra <saikiranra@gmail.com>
2023-01-25 21:14:53 -0800

    Deleted old CSS and JS, upgraded Jquery and Bootstrap.

--- footer.php
+++ footer.php
@@ -1,26 +1,7 @@
-<!--Footer-->
-    <footer class="page-footer orange">
-        <hr>
-    <!--Call to action-->
-       <div class="call-to-action">
-            <ul>  
-              <li class="nav-item active">
-                <a class="nav-link" href="index.php" style="color:Teal;"><b>Scouting 2023</b></a>
-            </li> 
-            </ul> 
-        </div> 
-        <!--/.Call to action-->
-        <!--Copyright-->
-       <!-- <div class="footer-copyright">
-            <div class="container-fluid">
-
-
-            </div>
-        </div>
-    
-    <span class="sr-only">(current)</span>
-    
-    -->
-        <!--/.Copyright-->
-    </footer>
-<!--/.Footer-->
+<script type="text/javascript" src="js/jquery-3.6.3.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.bundle.min.js"></script>
+<script>
+  $(document).ready(function(){
+    $.material.init();
+  });
+</script>
--- index.php
+++ index.php
@@ -1,29 +1,16 @@
-<html lang="en" class="full-height">
-<?php
-include('navbar.php');
-?>
+<title>Scouting</title>
+<html lang="en">
 
-<body style="background-color:#008080">
-  <!-- Main container-->
-  <div class="container">
-    <div class="divider-new pt-5">
-      <h2 style="color:White;">
-        <b>Index<b>
-      </h2>
-    </div> 
-    <!--Section: Best features-->
-    <section id="best-features">
-      <div class="row pt-3">
-      </div>
-    </section> 
-    <!--/Section: Best features-->
+<?php include('navbar.php'); ?>
+
+<body class="bg-body">
+  <div class="container row-offcanvas row-offcanvas-left">
+    <div class="well column  col-lg-12  col-sm-12 col-xs-12" id="content">
+      <h1>Main Page</h1>
+    </div>
   </div>
-  <!--/ Main container-->
-  <?php include("footer.php"); ?>
-  <!-- Animations init-->
-  <script>
-    new WOW().init();
-  </script>
 </body>
+
+<?php include("footer.php"); ?>
 
 </html>
