1e22db21323c1b8d92ced809f876c3ef96919a94
Sricharan Pamireddy <sricharan.pamireddy@gmail.com>
2023-02-19 18:21:18 -0800

    pictureUpload is now working. added pitPictureUpload to writeAPI.
    gitignore now ignores the uploads folder

--- pictureUpload.php
+++ pictureUpload.php
@@ -1,54 +1,5 @@
 <html>
 <?php include("navBar.php"); ?>
-<?php
-$target_dir = "uploads/";
-$target_file = $target_dir . $_POST["teamNumber"] . "." . time() . "." . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-
-// Check if image file is a actual image or fake image
-if(isset($_POST["teamNumber"])) {
-  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-  if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
-    $uploadOk = 1;
-  } else {
-    echo "File is not an image.";
-    $uploadOk = 0;
-  }
-}
-
-// Check if file already exists
-if (file_exists($target_file)) {
-  echo "Sorry, file already exists.";
-  $uploadOk = 0;
-}
-
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
-  echo "Sorry, your file is too large.";
-  $uploadOk = 0;
-}
-
-// Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-  $uploadOk = 0;
-}
-
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-  echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
-  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    echo "<script>alert('The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded');</script>";
-  } else {
-    echo "Sorry, there was an error uploading your file.";
-  }
-}
-?>
 <style>
     #overallForm {
         font-size: 15px;
@@ -57,6 +8,10 @@ if ($uploadOk == 0) {
 </style>
 
 <body class="bg-body">
+  <script>
+    const url = new URLSearchParams(window.location.search);
+    alert(url.get("message"));
+    </script>
     <div class="container row-offcanvas row-offcanvas-left">
         <div class="well column col-lg-12 col-sm-12 col-xs-12" id="content">
             <div class="row pt-3 pb-3 mb-3">
@@ -78,7 +33,7 @@ if ($uploadOk == 0) {
                                         Pit Scout Form
                                     </button>
                                 </a>
-                                <form action="" method="post" enctype="multipart/form-data">
+                                <form action="writeAPI.php" method="post" enctype="multipart/form-data">
                                     <div class="form-group">
                                         <text class="col-lg-2 control-label">Team Number: </text>
                                         <div class="col-lg-10">
@@ -89,6 +44,7 @@ if ($uploadOk == 0) {
                                     <div class="col-lg-12 col-sm-12 col-xs-12">
                                         Select images to upload:
                                         <input type="file" name="fileToUpload" multiple id="fileToUpload">
+                                        <input type="text" name="pitPictureUpload" value="1" hidden>
                                         <input id="PitScouting" type="submit" class="btn btn-primary" value="Submit Data" onclick="">
                                 </form>
                             </div>
