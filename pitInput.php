a5add596888ed2f8666fa27fc0d005cf4e994c7a
Sricharan Pamireddy <sricharan.pamireddy@gmail.com>
2023-02-19 21:24:42 -0800

    made the alert messages nicer by using html modals.

--- pitInput.php
+++ pitInput.php
@@ -1,8 +1,63 @@
 <html>
 <?php include("navBar.php"); ?>
 
+<head>
+	<style>
+		.modal {
+			display: none;
+			/* Hidden by default */
+			position: fixed;
+			/* Stay in place */
+			z-index: 1;
+			/* Sit on top */
+			padding-top: 100px;
+			/* Location of the box */
+			left: 0;
+			top: 0;
+			width: 100%;
+			/* Full width */
+			height: 100%;
+			/* Full height */
+			overflow: auto;
+			/* Enable scroll if needed */
+			background-color: rgb(0, 0, 0);
+			/* Fallback color */
+			background-color: rgba(0, 0, 0, 0.4);
+			/* Black w/ opacity */
+		}
+
+		/* Modal Content */
+		.modal-content {
+			background-color: #fefefe;
+			margin: auto;
+			padding: 20px;
+			border: 1px solid #888;
+			width: 80%;
+		}
+
+		/* The Close Button */
+		.close {
+			color: #aaaaaa;
+			float: right;
+			font-size: 28px;
+			font-weight: bold;
+		}
+
+		.close:hover,
+		.close:focus {
+			color: #000;
+			text-decoration: none;
+			cursor: pointer;
+		}
+	</style>
+</head>
 
 <body class="bg-body">
+	<div id="errorBox" class="modal">
+		<div id="alert-template" class="modal-content">
+			<span class="close" onclick="closeError(this)">&times;</span>
+		</div>
+	</div>
 	<div class="container row-offcanvas row-offcanvas-left">
 		<div class="well column col-lg-12 col-sm-12 col-xs-12" id="content">
 			<div class="row pt-3 pb-3 mb-3">
@@ -13,7 +68,7 @@
 						<div class="card-header">Pit Scout Form</div>
 						<div class="card-body">
 
-						<div id="alertPlaceholder"></div>
+							<div id="alertPlaceholder"></div>
 
 							<a href='pitCheck.php'>
 								<button class="btn btn-primary">
@@ -100,7 +155,7 @@
 		<?php include("footer.php"); ?>
 
 		<script>
-			function submitData(){
+			function submitData() {
 				/* Gets data from form, validates it, and creates appropriate error messages. 
 
 				Returns:
@@ -110,53 +165,61 @@
 				var data = getpitInputData();
 				console.log(data);
 				var validData = validateFormData(data);
-				if (validData){
+				if (validData) {
 					// Create POST request.
 					$.post("writeAPI.php", {
-						"writePitScoutData": JSON.stringify(data)
-					}, function(success) {
-						success = JSON.parse(success);
-						if (success){
-						createSuccessAlert('Form Submitted. Clearing form.');
-						location.reload();
-						}
-						else {
-						createErrorAlert('Form submitted to server but failed to process. Please try again or contact admin.');
-						}
-					})
-					.fail(function (){
-						createErrorAlert('Form submitted but failure on server side. Please try again or contact admin.');
-					});
+							"writePitScoutData": JSON.stringify(data)
+						}, function(success) {
+							console.log(success);
+							success = JSON.parse(success);
+							if (success) {
+								createSuccessAlert('Form Submitted. Clearing form.');
+								clearForm();
+							} else {
+								createErrorAlert('Form submitted to server but failed to process. Please try again or contact admin.');
+							}
+						})
+						.fail(function() {
+							createErrorAlert('Form submitted but failure on server side. Please try again or contact admin.');
+						});
 				}
 			}
 
+			var error = document.getElementById("errorBox");
+
+			window.onclick = function(event) {
+				if (event.target == error) {
+					error.style.display = "none";
+				}
+			}
+
+			function closeError(button) {
+				error.style.display = "none";
+			}
+
+			function createAlert(p) {
+				var template = document.getElementById("alert-template");
+				if (p.className == "error") template.style = "background-color: #ff6e6e;";
+				else template.style = "background-color: #6eff70;";
+				template.hidden = false;
+				template.appendChild(p);
+				error.innerText = "";  
+				error.appendChild(template);
+				error.style.display = "block";
+			}
+
 			function createErrorAlert(errorMessage) {
-				alert(errorMessage);
-				/* Creats a Error alert. 
-				
-				Args:
-				  successMessage: String of message to send.
-				*/
-				var alertValue = [`<div class="alert alert-danger alert-dismissible" role="alert">`,
-					`  <div>${errorMessage}</div>`,
-					`  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>`,
-					`</div>`
-				].join('');
-				$('#alertPlaceholder').append(alertValue);
+				var p = document.createElement("p");
+				p.innerText = errorMessage;
+				p.className = "error";
+				createAlert(p);
 			}
 
 			function createSuccessAlert(successMessage) {
-				/* Creats a success alert. 
-				
-				Args:
-				  successMessage: String of message to send.
-				*/
-				var alertValue = [`<div class="alert alert-success alert-dismissible" role="alert">`,
-					`  <div>${successMessage}</div>`,
-					`  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>`,
-					`</div>`
-				].join('');
-				$('#alertPlaceholder').append(alertValue);
+				var p = document.createElement("p");
+				p.innerText = successMessage;
+				p.className = "success";
+				createAlert(p);
 			}
 
 			function clearAlerts() {
@@ -201,7 +264,7 @@
 				data['pitTeamNumber'] = $('#pitTeamNumber').val();
 				data['pitTeamName'] = $('#pitTeamName').val();
 				data['disorganized'] = $('#disorganized').val();
-				data['numBatteries'] = parseInt($('#numBatteries').val())  || 0;
+				data['numBatteries'] = parseInt($('#numBatteries').val()) || 0;
 				data['chargedBatteries'] = parseInt($('#chargedBatteries').val()) || 0; // Either form input or 0 if no form input
 				data['codeLanguage'] = $('#codeLanguage').val(); // Either form input or 0 if no form input
 				data['autoPath'] = $('#autoPath').val(); // Either form input or 0 if no form input
@@ -211,12 +274,12 @@
 			}
 
 			$("#submit").on('click', function(event) {
-    		submitData();
-  			});
+				submitData();
+			});
 		</script>
 
 
-<style>
+		<style>
 			/* The container */
 			.container2 {
 				display: inline-block;
@@ -285,4 +348,5 @@
 				transform: rotate(45deg);
 			}
 		</style>
+
 </html>
