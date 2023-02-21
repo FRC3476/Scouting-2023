9f8261d84e4001627d220f5a5ee64996fe9114f5
Keyur157 <71665518+Keyur157@users.noreply.github.com>
2023-02-20 17:34:13 -0800

    Jump Scare

--- index.php
+++ index.php
@@ -6,11 +6,19 @@ include('navBar.php');
 
 <script src="js/bootstrap.min.js"></script>
 
+<style>
+body {
+  background-image: url('remy.png');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+}
+</style>
 
 
 <body style="background-color:#008080">
     <header>
-        <img  src="remy.png; no-repeat center center">
+        
     </header>
 </body>
 </html>
