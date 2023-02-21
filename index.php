a2d3b9276477dd1948c2a10dc999664d1c2dec01
Keyur157 <71665518+Keyur157@users.noreply.github.com>
2023-02-20 17:20:46 -0800

    index

--- index.php
+++ index.php
@@ -1,59 +1,16 @@
-<html lang="en" class="full-height">
-<?php include('navBar.php') ?>
+<html lang="en">
+<?php
+include('header.php');
+include('navBar.php'); 
+?>
+
 <script src="js/bootstrap.min.js"></script>
 
 
 
 <body style="background-color:#008080">
-<div style="background-image:url('images/remy.png');">
-  <h1>hello</h1>
-</div>
+    <header>
+        <img  src="remy.png; no-repeat center center">
+    </header>
 </body>
-<!--
-</body>
-        Intro Section
-        <section class="view intro-1 hm-black-strong">
-            <div style="background-color: rgba(0,0,0,.3);" class="full-bg-img flex-center">
-                <div class="container">
-                    <div class="col-lg-2 mb-r">
-                        <a href="MatchInput.php" class="btn btn-warning">Match Scout Input</a>
-                    </div>
-
-                    <div class="col-lg-2 mb-r">
-                        <a href="pitInput.php" class="btn btn-warning">Pit Scout Form</a>
-                    </div>
-
-                    <div class="col-lg-2 mb-r">
-                        <a href="pictureUpload.php" class="btn btn-warning">Picture Upload</a>
-                    </div>
-
-                    <div class="col-lg-2 mb-r">
-                        <a href="leadScoutInput.php" class="btn btn-warning">Lead Scout Form</a>
-                    </div>
-            </div>
-          </div>
-    </section>
-</body>
-<style>
-    /* TEMPLATE STYLES */
-    .flex-center {
-        color: #fff;
-    }
-
-    .intro-1 {
-        background: url("images/remy.png;");
-        background-size: cover;
-    }
-
-    .navbar .btn-group .dropdown-menu a:hover {
-        color: #000 !important;
-    }
-
-    .navbar .btn-group .dropdown-menu a:active {
-
-        color: #fff !important;
-    }
-</style>
-  -->
-
 </html>
