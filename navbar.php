30cd3748f959f4a7b90bbca8c66a3da400094550
Keyur157 <71665518+Keyur157@users.noreply.github.com>
2023-02-26 11:37:31 -0800

    added pit dropdown

--- navbar.php
+++ navbar.php
@@ -13,17 +13,27 @@
         <li class="nav-item">
           <a class="nav-link" href="simpleMatchForm.php">Match Form</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="pitInput.php">Pit Input</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="pictureUpload.php">Picture Upload</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="pitCheck.php">Pit Check</a>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Pit Scout Pages
+          </a>
+          <ul class="dropdown-menu">
+            <li>
+              <a class="dropdown-item" href="pitInput.php">Pit Input</a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="pictureUpload.php">Picture Upload</a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="pitCheck.php">Pit Check</a>
+            </li>
+          </ul>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="leadScoutForm.php">Lead Scout Form</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="teamData.php">Team Data</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="coprs.php">COPRS</a>
