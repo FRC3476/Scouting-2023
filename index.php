d0183a0b9d1ede6c7c09998817d74eff5d9a9646
saikiranra <saikiranra@gmail.com>
2023-03-03 03:09:35 -0800

    Added simple text to index.

--- index.php
+++ index.php
@@ -11,10 +11,24 @@ body {
 </style>
 
 
-<body style="background-color:#008080">
-    <header>
-        
-    </header>
+<body  style="background-color:#008080">
+  <div class="container row-offcanvas row-offcanvas-left">
+    <div class="well column col-lg-12 col-sm-12 col-xs-12" id="content">
+      <div class="row pt-3 pb-3 mb-3">
+        <div class="col-lg-12 col-sm-12 col-xs-12 gx-3">
+          <div class="card">
+            <div class="card-body">
+
+              <h1>2023 Scouting System</h1>
+              
+              Please reach out to 3476 for any urgent support!
+            
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </body>
 </html>
 
