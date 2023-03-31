06dcdd0b060dac9bce25057a985569b0b71bc26f
Abhinav Prasad <abhinavprasad51@yahoo.com>
2023-03-30 22:17:05 -0700

    made changes to pit scouting page

--- pitData.php
+++ pitData.php
@@ -1,4 +1,4 @@
-<title>Pit Data</title>
+  <title>Pit Data</title>
 <html lang="en">
 
 <?php include('navbar.php'); ?>
@@ -22,7 +22,7 @@
                     <th scope="col">Batteries</th>
                     <th scope="col">Chargers</th>
                     <th scope="col">Language</th>
-					<th scope="col">Drivetrain Type</th>
+					          <th scope="col">Drivetrain Type</th>
                     <th scope="col">Robot Weight</th>
                     <th scope="col">Frame Perimeter</th>
                     <th scope="col">Comments</th>
